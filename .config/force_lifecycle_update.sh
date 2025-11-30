7e3fddd1833d7624d3732a59db7bd77a5cf49c83
Aldo W. Chacon <aldochaconc@gmail.com>
2025-11-30 14:34:14 -0300

    omarchy based environment

--- .config/force_lifecycle_update.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-LOG_FILE="/tmp/lifecycle_manager.log"
-
-echo "$(date): Force update triggered." >> "${LOG_FILE}"
-rm -f "/home/crystal/.current_lifecycle_period" && \
-systemctl --user start lifecycle_manager.service && \
-echo "$(date): Force update actions completed." >> "${LOG_FILE}" || \
-echo "$(date): Force update actions failed." >> "${LOG_FILE}" 
